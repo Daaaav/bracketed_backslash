e9064815c530d1fece76aa1fc71b2313204f9f6c
Dav999 <dav999@tolp2.nl>
2016-10-14 20:42:15 +0200

    Forgot a backtick xd

--- intro.py
+++ intro.py
@@ -463,7 +463,7 @@ def on_member_join (member):
 		else:
 			# Not found, so they're just a tOLPer.
 			yield from client.add_roles(member, discord.utils.get(member.server.roles, id='231644869351833600')) # The tOLPer role
-			yield from client.send_message(specialchannel, '`Given {}`#{}` `({}) the tOLPer role.`'.format(member.name, member.discriminator, member.id))
+			yield from client.send_message(specialchannel, '`Given` {}`#{}` `({}) the tOLPer role.`'.format(member.name, member.discriminator, member.id))
 
 @client.async_event
 def on_member_remove(member):
