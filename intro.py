277c98dc33a77fd63a0d41e18f1e0500ea01bca0
Dav999 <dav999@tolp2.nl>
2016-10-16 00:06:47 +0200

    "Discriminator only" now also allows #

--- intro.py
+++ intro.py
@@ -580,7 +580,7 @@ def get_member_input(server, input):
 	5) Case-insensitive username 100% match
 	6) Case-insensitive nickname partial match
 	7) Case-insensitive username partial match
-	8) Discriminator only
+	8) Discriminator only (either with or without #)
 	
 	"""
 	# Is this a mention?
@@ -613,6 +613,8 @@ def get_member_input(server, input):
 				if mem.name.lower().find(input.lower()) != -1:
 					userfound = mem
 				if mem.discriminator == input:
+					discmatched = mem
+				if input.startswith('#') and mem.discriminator == input[1:]:
 					discmatched = mem
 
 			if not nickmatched:  # No 100% nickname match
