bf57524f895c8447cec048c73cf7b6ecbbe4c808
Info Teddy <infoteddyatgitgud@cock.li>
2016-10-17 16:50:19 -0700

    add very primitive version of caching attachments (also known as not caching them but echoing the link_

--- intro.py
+++ intro.py
@@ -48,6 +48,16 @@ def on_message(message):
 			yield from client.send_message(specialchannel, msg_start)
 		pass
 
+	if message.attachments != []:
+		if message.server.id != productionserver:
+			yield from client.send_typing(message.channel)
+		else:
+			yield from client.send_typing(specialchannel)
+		msg_start = '**`>`**`user` {}`#{}` `({}) attached a file to message {} in channel` <#{}> `at {} UTC`'.format(message.author.name, message.author.discriminator, message.author.id, message.id, message.channel.id, message.timestamp)
+		content = '_`The attachment is:`_\n' + message.attachments[0]['url']
+		msg = msg_start + content
+		yield from client.send_message(specialchannel, msg)
+
 	if message.content.startswith(invoker): # does the message start with command invoker
 		altinvokeractive = False
 		pass # continue, go on
