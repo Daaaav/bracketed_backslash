e2ceb0143e7dcbebd7dabbb4adda48771ad55922
Dav999 <dav999@tolp2.nl>
2016-11-01 01:49:03 +0100

    Minor fixes

--- intro.py
+++ intro.py
@@ -279,7 +279,7 @@ async def on_ready():
 				memberroles[str(mem.id)] = list(rolelist(mem.roles)) # Possibly redundant list() tbh, just making sure since I can't test and I don't know python well enough to know whether it's redundant
 				continue
 			if set(memberroles[str(mem.id)]) != set(rolelist(mem.roles)):
-				warnings += '\nUser {}#{} ({}) doesn\'t have the same roles they had when I last checked! Maybe you want to correct things.\n    Cached: {}\nSeen: {}'.format(mem.name, mem.discriminator, mem.id, listroles_id(memberroles[str(mem.id)]), listroles(mem.roles))
+				warnings += '\nUser {}#{} ({}) doesn\'t have the same roles they had when I last checked! Maybe you want to correct things.\n    Cached: {}\n    Seen: {}'.format(mem.name, mem.discriminator, mem.id, listroles_id(memberroles[str(mem.id)]), listroles(mem.roles))
 		if warnings != '':
 			warnings = '**User role cache warning**' + warnings
 			await client.send_message(specialchannel, warnings[:1900])
@@ -1386,7 +1386,7 @@ def rolecachesave():
 
 	with open('members.json', 'w') as outfile:
 		json.dump(memberroles, outfile)
-		
+
 def listroles(lijst):
 	returnage = ''
 	for role in lijst:
