c0c06f0031ad4c85f2b562be738c99188cdaf74c
Dav999 <dav999@tolp2.nl>
2016-11-12 03:01:45 +0100

    "The rules are now showing as "disabled" if there are rules" fixed

--- intro.py
+++ intro.py
@@ -967,7 +967,7 @@ async def on_message(message):
 			except IndexError:
 				pass
 		n = 1
-		content = 'Rules for server `{}`:{}'.format(mdspecialchars(message.server.name), ' (Disabled)' if message.server.id in rules else '')
+		content = 'Rules for server `{}`:{}'.format(mdspecialchars(message.server.name), ' (Disabled)' if message.server.id in disabledrules else '')
 		for rule in rules[message.server.id]:
 			content += '\n**{}.** {}'.format(n, rule)
 			n += 1
