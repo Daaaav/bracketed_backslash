b652cfcc33256c5d788669ced376523aa4c5a9da
Dav999 <dav999@tolp2.nl>
2016-11-19 02:12:53 +0100

    Made server-specific configs technically supported

--- config.py
+++ config.py
@@ -7,16 +7,33 @@ configs = {
 	'test': {
 		'default': 2,
 		'type': 'int',
-		'expl': 'A testing setting to see whether everything works'
+		'expl': 'A testing setting to see whether everything works',
+		'detachable': True
 	},
 	'timeformat': {
 		'default': '%Y-%m-%d %H:%M:%S (%Z)',
 		'type': 'str',
-		'expl': 'The date format used in messages.'
+		'expl': 'The date format used in messages.',
+		'detachable': True
 	},
 }
 
 s = {}
+
+def get_s(skey, serverid=None):
+	if serverid != None and serverid in s[key]:
+		return s[skey][serverid]
+	return s[skey]['master']
+
+def set_s(skey, value, serverid=None):
+	if serverid != None and serverid in s[key]:
+		s[skey][serverid] = value
+	s[skey]['master'] = value
+
+def is_detached(skey, serverid):
+	if serverid in s[key]:
+		return True
+	return False
 
 def saveconfig():
 	with open('config.json', 'w') as outfile:
@@ -24,7 +41,7 @@ def saveconfig():
 
 def loaddefaultsettings():
 	for default in configs:
-		s[default] = configs[default]['default']
+		s[default]['master'] = configs[default]['default']
 
 def load():
 	loaddefaultsettings()
@@ -35,7 +52,11 @@ def load():
 		
 		for loadedsetting in loadedconfig:
 			# Overwrite the defaults piece by piece
-			s[loadedsetting] = loadedconfig[loadedsetting]
+			s[loadedsetting] = {}
+			s[loadedsetting]['master'] = loadedconfig[loadedsetting]['master']
+			for loadedlocalsetting in loadedconfig[loadedsetting]:
+				if loadedlocalsetting != 'master':
+					s[loadedsetting][loadedlocalsetting] = loadedconfig[loadedsetting][loadedlocalsetting]
 	except FileNotFoundError:
 		print('Making new config file')
 		saveconfig()
