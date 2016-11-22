cf1289f0ba9e0f22bab881f1cff86a94b0fe55a8
Info Teddy <infoteddyatgitgud@cock.li>
2016-11-22 10:07:13 -0800

    replace a `_(none)_` string with `No Nickname`

--- intro.py
+++ intro.py
@@ -977,7 +977,7 @@ async def on_message(message):
 				displaygameurl = '``{}``'
 		embed = discord.Embed(colour=targetmember.colour)
 		embed.set_thumbnail(url=targetmember.avatar_url)
-		embed.add_field(name='Nickname' if targetmember.nick != None else 'No Nickname', value='``{}``'.format(mdspecialchars(targetmember.nick)) if targetmember.nick != None else '_(none)_')
+		embed.add_field(name='Nickname' if targetmember.nick != None else 'No Nickname', value='``{}``'.format(mdspecialchars(targetmember.nick)) if targetmember.nick != None else 'No Nickname')
 		embed.add_field(name='Username', value='``{}``'.format(mdspecialchars(targetmember.name)))
 		embed.add_field(name='Discriminator', value='#{}'.format(targetmember.discriminator))
 		embed.add_field(name='User ID', value=targetmember.id)
