f8c96e46df22c3255438cb5180c6b096e97731bf
Info Teddy <infoteddyatgitgud@cock.li>
2016-11-24 14:30:40 -0800

    remove mixed indentation [2]

--- main.py
+++ main.py
@@ -1546,8 +1546,8 @@ async def on_message(message):
 			try:
 				cmdbits = arguments.split() # should split it so [1] is number, [2] is operand, [3] is third number
 				if cmdbits[0] == "":
-				    cmdbits = {"", "", ""}
-				    out = "No input found."
+					cmdbits = {"", "", ""}
+					out = "No input found."
 					raise ValueError() # They've entered nothing, raise an exception
 				cmdbits[0] = float(cmdbits[0])
 				cmdbits[2] = float(cmdbits[2])
