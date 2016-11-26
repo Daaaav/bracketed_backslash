e0fb40bd684a4789ed6969d1d68c05c364f89bb9
Info Teddy <infoteddyatgitgud@cock.li>
2016-11-25 17:59:05 -0800

    handle `message` being `None`

--- functions.py
+++ functions.py
@@ -129,6 +129,8 @@ def get_member_input(server, input):
 @client.event
 async def reply(messageobject, message, emb=None):
 	# Removes the need for adding msg_start manually every time
+	if message == None:
+		message = ''
 	if len(msg_start + message) >= 2000:
 		# We can at least try in a totally not failsafe and kinda ugly way
 		content = msg_start + message
