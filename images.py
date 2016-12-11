00675428d8833ac9c5db7b4b148fea7b06411b56
Dav999 <dav999@tolp2.nl>
2016-12-11 02:33:45 +0100

    Consistency

--- images.py
+++ images.py
@@ -9,7 +9,7 @@ redbarim = Image.open('img/redbar.png')
 thresholdim = Image.open('img/threshold.png')
 
 def progressbar(percentage):
-	tempimg = Image.new("RGBA", (102,12))
+	tempimg = Image.new('RGBA', (102,12))
 	tempimg.paste(barim, (0,0,102,12))
 
 	if percentage >= 95:
@@ -19,9 +19,9 @@ def progressbar(percentage):
 	else:
 		barcolor = greenbarim
 	tempimg.paste(barcolor.crop((0,0,percentage,10)), (1,1,percentage+1,11))
-	tempimg.save("img/temp.png") # Maybe we can write to and return a file-like object instead of writing anything to disk?
+	tempimg.save('img/temp.png') # Maybe we can write to and return a file-like object instead of writing anything to disk?
 
-	return "img/temp.png"
+	return 'img/temp.png'
 
 def votebar(percentagepro, percentageopp, threshold):
 	tempimg = Image.new('RGBA', (102,12))
