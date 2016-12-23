75a502c9b469b70de40619c9a424e8e7fab7c975
Info Teddy <infoteddyatgitgud@cock.li>
2016-12-22 20:03:46 -0800

    make `\kill` exit with custom exit code

--- main.py
+++ main.py
@@ -837,6 +837,7 @@ async def on_message(message):
 		logcommand(command, arguments, message)
 		await reply(message, emb=embed)
 		await client.logout()
+		sys.exit(1)
 	elif command == 'config':
 		if not is_operator(message.author):
 			embed = emb.error(t['op_only'])
