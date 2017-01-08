6659901755e9e94a5d62d2d840cb96ffaed5d46b
Info Teddy <infoteddyatgitgud@cock.li>
2017-01-08 12:09:09 -0800

    Fix TypeError in on_reaction_remove()
    The full error description was:
    
      TypeError: set_author() takes 1 positional argument but 2 positional arguments (and 2 keyword-only arguments) were given

--- main.py
+++ main.py
@@ -2200,7 +2200,7 @@ async def on_reaction_remove(r, u):
 		colour=u.colour,
 	)
 	embed.set_author(
-		u.display_name,
+		name=u.display_name,
 		icon_url=u.avatar_url,
 		url=infourl('userid={}&messageid={}'.format(u.id, r.message.id))
 	)
