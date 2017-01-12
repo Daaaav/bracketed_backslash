fadab16f611ec464aa90a053d0633e985b927f9b
Dav999 <dav999@tolp2.nl>
2017-01-12 23:11:08 +0100

    Add \tntgb_maint with liftmsg
    A maintenance toolbox function for TNTGB.
    
    Also changed syntax.bat a bit.

--- syntax.bat
+++ syntax.bat
@@ -1,6 +1,6 @@
+@echo off
 REM  I guess I could .gitignore this? Ah well, it's probably useful for any developer using Windows and who has Pyflakes.
 
-@echo off
 REM  Unfortunately Pyflakes doesn't understand exec(compile(open().read()))
 (
 	type main.py
@@ -10,8 +10,8 @@ REM  Unfortunately Pyflakes doesn't understand exec(compile(open().read()))
 	type commands.py
 ) >concatenatedmain.py
 
-@echo on
 REM  Nor import. But hey, this works.
+@echo on
 C:\Users\U\AppData\Local\Programs\Python\Python35\Scripts\pyflakes concatenatedmain.py col.py config.py emb.py images.py server_configs.py
 
 @echo off
