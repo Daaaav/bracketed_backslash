283db34c653b1564b0bdd32d209bc6eb29343f6a
Info Teddy <infoteddyatgitgud@cock.li>
2017-01-12 14:47:23 -0800

    Make reaction add logs use name, not display_name
    Since it displays the member in Discord tag form
    (USERNAME#DISCRIMINATOR), it should display the user’s username, and not
    the user’s display name.

--- main.py
+++ main.py
@@ -1018,7 +1018,7 @@ async def on_reaction_add(r, u):
 		url=infourl('userid={}&messageid={}'.format(u.id, r.message.id))
 	)
 	mdetails = '**{name}**#{discrim}'.format(
-		name=mdspecialchars(u.display_name),
+		name=mdspecialchars(u.name),
 		discrim=u.discriminator,
 	)
 	if u.status == discord.Status.offline:
