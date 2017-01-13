89a38551e58c4ba9587af86dd544e8376f99ea43
Info Teddy <infoteddyatgitgud@cock.li>
2017-01-13 10:24:01 -0800

    Make on_channel_create() properly check if DM
    Makes on_channel_create() properly check if the channel type is of a
    PrivateChannel. This fixes a bug where an AttributeError would pop up in
    the terminal output every time a direct message was created with the
    bot.

--- main.py
+++ main.py
@@ -1233,7 +1233,7 @@ async def on_voice_state_update(before, after):
 
 @client.event
 async def on_channel_create(c):
-	if isprivatemessage(c) or logdisabled('channel_add', c.server):
+	if c.type == discord.ChannelType.private or logdisabled('channel_add', c.server):
 		return
 	schan = getspecialchannel(c.server)
 	embed = discord.Embed(
