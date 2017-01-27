e64fcee659308b84d740949e98ccf023ad57b4a7
Dav999 <dav999@tolp2.nl>
2017-01-27 18:59:43 +0100

    Make output of syntax.bat much clearer

--- syntax.bat
+++ syntax.bat
@@ -1,6 +1,8 @@
 @echo off
 REM  I guess I could .gitignore this? Ah well, it's probably useful for any developer using Windows and who has Pyflakes.
 
+echo ----
+
 REM  Unfortunately Pyflakes doesn't understand exec(compile(open().read()))
 (
 	type main.py
@@ -11,10 +13,9 @@ REM  Unfortunately Pyflakes doesn't understand exec(compile(open().read()))
 ) >concatenatedmain.py
 
 REM  Nor import. But hey, this works.
-@echo on
 C:\Users\U\AppData\Local\Programs\Python\Python35\Scripts\pyflakes concatenatedmain.py col.py config.py emb.py images.py
 
-@echo off
+echo ----
 pause
 
 del concatenatedmain.py
