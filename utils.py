e19f55a15c3e157e1b2e6c7292f9ed57fa6d77fb
Info Teddy <infoteddyatgitgud@cock.li>
2017-03-05 16:14:43 -0800

    utils.py: Add id_summary()
    For putting IDs in rich embed footers in logs, as using infourl() means
    putting IDs in URLs, which means they can't be searched by Discord's
    search, because Discord search doesn't search inside rich embed URLs.

--- utils.py
+++ utils.py
@@ -12,3 +12,16 @@ def mdspecialchars(string, character='\\'):
 		return string
 	except AttributeError:
 		return string
+
+def id_summary(uid=None, mid=None, cid=None):
+	"""Return a oneline summary of IDs."""
+	summary = ''
+	if uid:
+		summary += ' \N{BUST IN SILHOUETTE}' + uid
+	if mid:
+		summary += ' \N{SPEECH BALLOON}' + mid
+	if cid:
+		summary += ' \N{TELEVISION}' + cid
+	if summary.startswith(' '):
+		summary = summary[1:]
+	return summary
