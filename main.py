ee3c4554b0d8af52c1960a484fe8b0658b624b5e
Info Teddy <infoteddyatgitgud@cock.li>
2017-03-15 20:10:02 -0700

    main.py: Add opserver_botservers global var
    This is the channel where the bot being added and removed from servers
    is logged. I'm not using global vars because I just want to get this
    done first, and I'm too tired.

--- main.py
+++ main.py
@@ -142,7 +142,7 @@ maineventloop = asyncio.get_event_loop()
 
 @client.event
 async def on_ready():
-	global memberroles, rules, disabledrules, server, specialchannel_prod, botschannel, voicetextchannel, botschannel_tntgb, joinchannel_tntgb, banlogchannel_tntgb, productionserver, tntgbserver, rolexpires, opserver
+	global memberroles, rules, disabledrules, server, specialchannel_prod, botschannel, voicetextchannel, botschannel_tntgb, joinchannel_tntgb, banlogchannel_tntgb, productionserver, tntgbserver, rolexpires, opserver, opserver_botservers
 	productionserver = '153368829160849408'
 	tntgbserver = '242099933665034240'
 	server = discord.utils.get(client.servers, id=productionserver) # defines all server.* commands
@@ -154,6 +154,10 @@ async def on_ready():
 	joinchannel_tntgb = discord.utils.get(server_tntgb.channels, id='266591127644143618')
 	banlogchannel_tntgb = discord.utils.get(server_tntgb.channels, id='242253449922609152')
 	opserver = discord.utils.find(lambda s: s.id == opserverid, client.servers)
+	opserver_botservers = discord.utils.find(
+		lambda c: c.id == '291764490578558977',
+		opserver.channels,
+	)
 	logging.info('logged in as {} with id {}'.format(client.user.name, client.user.id))
 	await client.change_presence(game=discord.Game(name=config.get_s('gamestatus')))
 	embed = discord.Embed(title='🔌BOT CONNECTED', colour=server.me.colour)
