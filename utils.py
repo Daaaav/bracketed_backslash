6d586fe6dd0edb9231e11cc0b9906ef05d353334
Info Teddy <infoteddyatgitgud@cock.li>
2017-03-19 19:11:26 -0700

    utils.py: Add match_input()
    match_input() is a universal get_member_input(), and was made because we
    needed a universal get_member_input(), to be able to say "feels good
    when we don't have to type an entire ID or name with case sensitivity"
    to things other than members.

--- utils.py
+++ utils.py
@@ -117,3 +117,124 @@ async def handle_delete_overedited_message(msg, schan):
 				'{0.author.mention}. Were you going to stop editing that message?'
 			).format(msg),
 		)
+
+def match_input(objtype, request, *, server=None, client=None):
+	"""Return a member/server/channel/role/emoji object given an input which could be anything
+	that identifies that object. If it can't be found, return None.
+
+	objtype: A string that specifies the type of object to search for. Can be either 'member',
+	'server', 'channel', 'role', or 'emoji'.
+
+	request: A string that will be tried to be matched to.
+
+	server (optional): A discord.Server object, which is the scope of where
+	members/channels/roles/emojis will be searched. Defaults to None.
+
+	client (optional): A discord.Client object, which is the scope of where servers will be
+	searched. Defaults to None.
+
+	The following priority is used:
+	1) (Members only) Mention: <@146814960574398464> or <@!146814960574398464>
+	2) (Channels only) Mention: <#153368829160849408>
+	3) (Roles only) Mention: <@&153369506813706240>
+	4) (Emojis only) Emoji: <:unjoy:263889385492185088>
+	5) ID: 146814960574398464
+	6) (Members only) Username/Username+Discriminator (Discord tag)/Nickname, whatever
+	discord.Server.get_member_named() accepts: Info Teddy, Info Teddy#3737, info teddy
+	7) Name: tOLP, general, Owner, unjoy
+	8) (Members only) Case-insensitive nickname complete match: info teddy
+	9) Case-insensitive name complete match: Info Teddy, tolp, owner
+	10) (Members only) Case-insensitive nickname partial match: info
+	11) Case-insensitive name partial match: Info, tOL, own
+	12) (Members only) Discriminator only (either with or without #): 3737
+	"""
+	acceptvals = ('member', 'server', 'channel', 'role', 'emoji')
+	if objtype not in acceptvals:
+		raise ValueError('objtype has to be one of ' + str(acceptvals))
+
+	# Is there a Discord server in between us, did we get a client,
+	# or is the request anything at all?
+	if (not server and objtype != 'server') or \
+	(not client and objtype == 'server') or \
+	not request:
+		return None
+
+	# Is this a mention, or an emoji? If so, extract the ID from it
+	if request.startswith('<') and request.endswith('>'):
+		if (objtype == 'member' and request[1] == '@') or \
+		(objtype == 'channel' and request[1] == '#'):
+			request = request[2:-1]
+		elif (objtype == 'member' and request[1:3] == '@!') or \
+		(objtype == 'role' and request[1:3] == '@&'):
+			request = request[3:-1]
+		elif objtype == 'emoji' and request[1] == request[-20] == ':':
+			request = request[-19:-1]
+
+	# Now get the object from the ID
+	tgt = server.get_member(request) if objtype == 'member' else \
+	client.get_server(request) if objtype == 'server' else \
+	server.get_channel(request) if objtype == 'channel' else \
+	discord.utils.find(lambda r: r.id == request, server.roles) if objtype == 'role' else \
+	discord.utils.find(lambda e: e.id == request, server.emojis) if objtype == 'emoji' else None
+
+	if not tgt:
+		# Not an ID
+
+		if objtype == 'member':
+			tgt = server.get_member_named(request) if objtype == 'member' else tgt
+
+			if not tgt:
+				# Not found by server.get_member_named()
+
+				# Everything else fails? Then try searching.
+				# Nicknames have priority, then usernames.
+				# Maybe we're entering just a discriminator, match those as well.
+				nickmatched = False
+				usermatched = None
+				nickfound = None
+				userfound = None
+				discmatched = None
+				for mem in server.members:
+					if mem.nick and mem.nick.lower() == request.lower():
+						tgt = mem
+						nickmatched = True
+						break
+					if mem.name.lower() == request.lower():
+						usermatched = mem
+						break
+					if mem.nick and \
+					mem.nick.lower().find(request.lower()) != -1:
+						nickfound = mem
+						break
+					if mem.name.lower().find(request.lower()) != -1:
+						userfound = mem
+						break
+					if mem.discriminator == request or \
+					(request.startswith('#') and \
+					mem.discriminator == request[1:]):
+						discmatched = mem
+						break
+				tgt = nickmatched if nickmatched else \
+				usermatched if usermatched else \
+				nickfound if nickfound else \
+				userfound if userfound else \
+				discmatched
+
+		else:
+			namematched = None
+			namefound = None
+
+			searchthru = client.servers if objtype == 'server' else \
+			server.channels if objtype == 'channel' else \
+			server.roles if objtype == 'role' else \
+			server.emojis if objtype == 'emoji' else None
+			for i in searchthru:
+				if i.name.lower() == request.lower():
+					namematched = i
+					break
+				if i.name.lower().find(request.lower()) != -1:
+					namefound = i
+					break
+			tgt = namematched if namematched else namefound
+
+	return tgt
