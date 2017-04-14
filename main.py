92159e9514e41d52476bbb3cfcd76bc6fb379be0
Info Teddy <infoteddyatgitgud@cock.li>
2017-04-14 15:26:03 -0700

    main.py: Add checking for allowedchannels
    on_message() now takes the config option for allowedchannels into
    account.

--- main.py
+++ main.py
@@ -500,6 +500,7 @@ async def on_message(m):
 	m.server.id == events.productionserver and \
 	not (is_dev(m.author) and m.channel.id == '238423391571279872') and \
 	not command in ['rule', 'rules', 'rulefind', 'rulesfind'] and \
+	not m.channel.id in config.get_s('allowedchannels', m.server.id) and \
 	not (m.channel.id == '256924583737819146' and command in ['votevoicemute', 'vy', 'vn']):
 		if is_valid_command(command) and command != '':
 			await client.add_reaction(
