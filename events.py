96fa331cabf083cdceb9bfde756417e3c7a528ae
Info Teddy <infoteddyatgitgud@cock.li>
2017-04-17 17:29:50 -0700

    Move main.on_reaction_add() to events.py
    Move main.on_reaction_add() to events.py, while maintaining all
    references to variables needed from that function.
    
    Having all events in a module makes it easier to reload them.

--- events.py
+++ events.py
@@ -1042,3 +1042,50 @@ async def on_server_role_update(before, after):
 			value=str(dict(after.permissions)[diff[0][0]]),
 		)
 		await __main__.client.send_message(specialchannel, embed=e)
+
+async def on_reaction_add(r, u):
+	if __main__.isprivatemessage(r.message.server) or __main__.logdisabled('reaction_add', r.message.server):
+		return
+	specialchannel = __main__.getspecialchannel(r.message.server)
+	try:
+		iscustomemote = True
+		emotename = r.emoji.name
+	except AttributeError:
+		iscustomemote = False
+		emotename = r.emoji
+	embed = discord.Embed(
+		title='REACTION ADDED TO MESSAGE (SENT {rtime} IN {c.mention})'.format(
+			rtime=__main__.reltime(time.mktime(r.message.timestamp.timetuple())),
+			c=r.message.channel,
+		),
+		description=r.message.content,
+		colour=u.colour,
+	)
+	embed.set_author(
+		name=u.display_name,
+		icon_url=u.avatar_url,
+		url=__main__.infourl('userid={}&messageid={}'.format(u.id, r.message.id))
+	)
+	mdetails = u.mention
+	if u.status == discord.Status.offline:
+		mdetails += ' (Invisible)'
+	embed.add_field(
+		name='Member of Reaction',
+		value=mdetails,
+	)
+	embed.add_field(
+		name='Reaction',
+		value=(
+			(emotename)
+			if
+			(not iscustomemote)
+			else
+			(
+				'{name} ({id})'.format(
+					name=str(r.emoji),
+					id=r.emoji.id,
+				)
+			)
+		),
+	)
+	await __main__.client.send_message(specialchannel, embed=embed)
--- main.py
+++ main.py
@@ -146,54 +146,6 @@ def shadow(auth=None, aliases=None, servonly=False):
 	return living_shadow
 
 @client.event
-async def on_reaction_add(r, u):
-	if isprivatemessage(r.message.server) or logdisabled('reaction_add', r.message.server):
-		return
-	specialchannel = getspecialchannel(r.message.server)
-	try:
-		iscustomemote = True
-		emotename = r.emoji.name
-	except AttributeError:
-		iscustomemote = False
-		emotename = r.emoji
-	embed = discord.Embed(
-		title='REACTION ADDED TO MESSAGE (SENT {rtime} IN {c.mention})'.format(
-			rtime=reltime(time.mktime(r.message.timestamp.timetuple())),
-			c=r.message.channel,
-		),
-		description=r.message.content,
-		colour=u.colour,
-	)
-	embed.set_author(
-		name=u.display_name,
-		icon_url=u.avatar_url,
-		url=infourl('userid={}&messageid={}'.format(u.id, r.message.id))
-	)
-	mdetails = u.mention
-	if u.status == discord.Status.offline:
-		mdetails += ' (Invisible)'
-	embed.add_field(
-		name='Member of Reaction',
-		value=mdetails,
-	)
-	embed.add_field(
-		name='Reaction',
-		value=(
-			(emotename)
-			if
-			(not iscustomemote)
-			else
-			(
-				'{name} ({id})'.format(
-					name=str(r.emoji),
-					id=r.emoji.id,
-				)
-			)
-		),
-	)
-	await client.send_message(specialchannel, embed=embed)
-
-@client.event
 async def on_reaction_remove(r, u):
 	if isprivatemessage(r.message.server) or logdisabled('reaction_remove', r.message.server):
 		return
