12cf01ae810da41bb70aec6f4f70c83238344125
Dav999 <dav999@tolp2.nl>
2017-05-13 22:30:33 +0200

    Add an error if a voice channel is matched in `\archive`

--- commands.py
+++ commands.py
@@ -2427,6 +2427,11 @@ async def archive(client, message, **kwargs):
 		await reply(message, emb=em)
 		return
 
+	if chan.type != discord.ChannelType.text:
+		em = emb.error('Matched a voice channel - text-to-speech is not implemented.')
+		await reply(message, emb=em)
+		return
+
 	lim = 100
 	if len(splitargs) >= 2:
 		try:
