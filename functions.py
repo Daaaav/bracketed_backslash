74efeeb487d5a696348e6370e1f008f59909b517
Info Teddy <infoteddyatgitgud@cock.li>
2017-06-04 12:14:21 -0700

    Move functions.is_dev() to main.py
    To remove the usage of exec-file.

--- functions.py
+++ functions.py
@@ -21,13 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # every function below here is custom-defined and not a part of discord.py
 
-def is_dev(member):
-	# Alright then. [2]
-	for role in member.roles:
-		if role.id == '238424544379928576': # [\] dev role
-			return True
-	return False
-
 def is_operator(member):
 	return member.id in op_ids.ids['operators'] or member.id == op_ids.ids['host']
 
