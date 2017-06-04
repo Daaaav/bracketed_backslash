e9b67b80f79cd1ac4af2207e2a4dc38e4a179f74
Info Teddy <infoteddyatgitgud@cock.li>
2017-06-04 12:56:19 -0700

    checks.py: Add op_ids import
    This fixes a NameError with is_operator() and is_host().

--- checks.py
+++ checks.py
@@ -19,6 +19,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import op_ids
+
 def is_admin(member):
 	try:
 		perms = member.server_permissions
