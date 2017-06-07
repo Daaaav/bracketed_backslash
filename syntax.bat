0bc62333d3535de74e07bd0148b0dec8c9356aa4
Dav999 <dav999@tolp2.nl>
2017-06-07 21:29:44 +0200

    Update syntax.sh and syntax.bat for new [\] structure
    functions.py was removed, and commands.py was turned into a module instead
    of being "included". These changes weren't reflected in the syntax
    checking shell/batch scripts.

--- syntax.bat
+++ syntax.bat
@@ -3,20 +3,10 @@ REM  I guess I could .gitignore this? Ah well, it's probably useful for any deve
 
 echo ----
 
-REM  Unfortunately Pyflakes doesn't understand exec(compile(open().read()))
-(
-	type main.py
-	echo.
-	type functions.py
-	echo.
-	type commands.py
-) >concatenatedmain.py
-
-REM  Nor import. But hey, this works.
-forfiles /m *.py /c "cmd /c if not @file==\"main.py\" if not @file==\"functions.py\" if not @file==\"commands.py\" (C:\Users\U\AppData\Local\Programs\Python\Python35\Scripts\pyflakes @file)"
+REM  Unfortunately Pyflakes doesn't understand import. But hey, this works.
+forfiles /m *.py /c "cmd /c C:\Users\U\AppData\Local\Programs\Python\Python35\Scripts\pyflakes @file"
 
 echo ----
 pause
 
-del concatenatedmain.py
 @echo on
--- syntax.sh
+++ syntax.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-printf "Creating concatenatedmain.py file..."
-
-cat main.py functions.py commands.py > concatenatedmain.py
-
-printf " Created!\n"
-
 for i in $( ls *.py ); do
-	if [[
-		"$i" != "main.py" &&
-		"$i" != "functions.py" &&
-		"$i" != "commands.py"
-	]]; then
-		printf "Running ${i} through pyflakes...\n"
-		pyflakes "$i"
-	else
-		printf "Skipped ${i}.\n"
-	fi
+	printf "Running ${i} through pyflakes...\n"
+	pyflakes "$i"
 done
-
-rm concatenatedmain.py
