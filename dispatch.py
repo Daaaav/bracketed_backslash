9968a2cb9419184f3e898ddda9110bbb6b60eaec
Info Teddy <infoteddyatgitgud@cock.li>
2017-11-20 18:07:10 -0800

    Add repeat_async_function()
    Does what it says on the tin. Repeat an asynchronous function - forever.
    This is useful in numerous ways, like, for example, periodically caching
    invites to keep the invite cache updated.

--- dispatch.py
+++ dispatch.py
@@ -35,3 +35,18 @@ async def dispatch(client, coro):
 			await client.on_error('bot_dispatch')
 		except asyncio.CancelledError:
 			pass
+
+def repeat_async_forever(client, function, args=None, kwargs=None, *, interval):
+	# As of right now, there is no intended way to stop it from repeating forever once it's run
+	# Fun side effect to note: it waits for the function to return before doing the delay again
+	if args is None:
+		args = []
+	if kwargs is None:
+		kwargs = {}
+
+	async def inner(inner_function, args, kwargs):
+		while True:
+			await inner_function(*args, **kwargs)
+			await asyncio.sleep(interval)
+
+	run(client, inner(function, args, kwargs))
