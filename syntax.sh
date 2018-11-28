b85f22a47d1f942d0f57fd2dec67982497457bd9
Dav999 <dav999@tolp2.nl>
2018-11-28 01:07:28 +0100

    Make syntax.sh output clearer
    The text "Running (file) through pyflakes..." is now color-coded gray,
    so that actual output from pyflakes (or from the shell) stands out
    more.

--- syntax.sh
+++ syntax.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+if [[ -t 1 ]]; then
+	GRAY='\033[90m'
+	NOCOLOR='\033[0m'
+else
+	GRAY=''
+	NOCOLOR=''
+fi
+
 for i in $( ls *.py ); do
-	printf "Running ${i} through pyflakes...\n"
+	printf "${GRAY}Running ${i} through pyflakes...${NOCOLOR}\n"
 	pyflakes3 "$i"
 done
