85b075084600eaacaaf43b03e8105fb1f178f084
Misa <infoteddyatgitgud@cock.li>
2020-08-19 15:17:24 -0700

    Update copyright years to 2020

--- op_ids.py
+++ op_ids.py
@@ -3,7 +3,7 @@
 
 """
 [\] bot, will be used for tolp server
-Copyright (C) 2016  Info Teddy
+Copyright (C) 2020  Info Teddy
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
